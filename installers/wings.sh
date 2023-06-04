bb9324b79cbb0c45e386ced0262b239b0ebc4d02
superJK92iscool <50285310+superJK92iscool@users.noreply.github.com>
2023-06-04 00:39:21 -0500

    Update wings.sh

--- installers/wings.sh
+++ installers/wings.sh
@@ -75,7 +75,7 @@ dep_install() {
   [ "$CONFIGURE_FIREWALL" == true ] && install_firewall && firewall_ports
 
   case "$OS" in
-  ubuntu | debian)
+  pop | debian)
     install_packages "ca-certificates gnupg lsb-release"
 
     mkdir -p /etc/apt/keyrings
@@ -176,7 +176,7 @@ configure_mysql() {
     echo "* Changing MySQL bind address.."
 
     case "$OS" in
-    debian | ubuntu)
+    debian | pop)
       sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
       ;;
     rocky | almalinux)
