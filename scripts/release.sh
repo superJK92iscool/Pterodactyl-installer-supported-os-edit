d1bd3c844a3f6afcadc7fdfa2fea74bfd68a37c0
superJK92iscool <50285310+superJK92iscool@users.noreply.github.com>
2023-06-04 00:38:32 -0500

    Update release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -34,7 +34,7 @@ git push
 
 output "Release $RELEASE pushed"
 
-output "Create a new release, with changelog below - https://github.com/pterodactyl-installer/pterodactyl-installer/releases/new"
+output "Create a new release, with changelog below - https://github.com/superJK92iscool/Pterodactyl-installer-supported-os-edit/releases/new"
 output ""
 
 changelog=$(scripts/changelog_parse.py)
